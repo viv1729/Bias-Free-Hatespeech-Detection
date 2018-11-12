9b23bb322897b192ddc0db262eb9b53559ec278a
vivenkyan <6823372+vivenkyan@users.noreply.github.com>
2018-11-12 23:42:31 +0530

    Added Gradient Reversal Layer.

--- data_handler.py
+++ data_handler.py
@@ -4,47 +4,60 @@ import codecs
 import pdb
 import pandas as pd
 import os
+import numpy as np
 
-dir_path = os.path.dirname(os.path.realpath(__file__))
+#dir_path = os.path.dirname(os.path.realpath(__file__))
+
+encoded_data_path = "encoded_data/"
 
 def get_data():
     tweets = []
-    files = ['racism.json', 'neither.json', 'sexism.json']
+    #files = ['racism.json', 'neither.json', 'sexism.json']
+    files = ['racism.json', 'sexism.json', 'amateur_expert.json']
     for file in files:
-        with codecs.open(dir_path + '/tweet_data/' + file, 'r', encoding='utf-8') as f:
+        with open('dataset/' + file, 'r', encoding='utf-8') as f:
             data = f.readlines()
+        
         for line in data:
             tweet_full = json.loads(line)
             tweets.append({
                 'id': tweet_full['id'],
                 'text': tweet_full['text'].lower(),
-                'label': tweet_full['Annotation'],
-                'name': tweet_full['user']['name'].split()[0]
+                'label': tweet_full['Annotation'].lower(),
+                'name': tweet_full['user']['name'].split()[0].lower()
                 })
 
-    #pdb.set_trace()
     return tweets
 
-def get_data__dataframe():
-    return pd.DataFrame.from_dict(data=get_data())
+
+def get_data_dataframe(data):
+    return pd.DataFrame.from_dict(data)
 
 
 
-if __name__=="__main__":
-    tweets = get_data()
-    males, females = {}, {}
-    with open(dir_path + '/tweet_data/males.txt') as f:
-        males = set([w.strip() for w in f.readlines()])
-    with open(dir_path + '/tweet_data/females.txt') as f:
-        females = set([w.strip() for w in f.readlines()])
+tweets = get_data()
+tweets_df = get_data_dataframe(tweets)
 
-    males_c, females_c, not_found = 0, 0, 0
-    for t in tweets:
-        if t['name'] in males:
-            males_c += 1
-        elif t['name'] in females:
-            females_c += 1
-        else:
-            not_found += 1
-    print males_c, females_c, not_found
-    pdb.set_trace()
+# saving the dataframe
+tweets_df.to_pickle(encoded_data_path + "tweets.pkl")
+
+males, females = {}, {}
+
+with open('dataset/' + 'males.txt', 'r', encoding='utf-8') as f:
+    males = set([w.strip().lower() for w in f.readlines()])
+    
+with open('dataset/' + 'females.txt', 'r',  encoding='utf-8') as f:
+    females = set([w.strip().lower() for w in f.readlines()])
+
+males_c, females_c, not_found = 0, 0, 0
+
+for t in tweets:
+    if t['name'] in males:
+        males_c += 1
+    elif t['name'] in females:
+        females_c += 1
+    else:
+        not_found += 1
+print(males_c, females_c, not_found) #714 535 11066
+    
+    
